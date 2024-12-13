a7c7e4c43931d9b7405f87c3efa467827d3c6239
Jakub Pronobis <jakub.pronobis@gmail.com>
2024-12-13 16:29:14 +0000

    first commit

---
+++ Truncation.Tests/TruncationTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Truncation;
+
+namespace Truncation.Tests
+{
+    public class TruncationTests
+    {
+        [Test]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            Assert.AreEqual(4, Truncation.Add(1, 3));
+        }
+    }
+}
---
+++ Truncation/Truncation.cs
@@ -0,0 +1,10 @@
+﻿namespace Truncation
+{
+    public class Truncation
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
